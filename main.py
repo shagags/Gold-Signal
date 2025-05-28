7811f1f4ec09d8861a4ba5802d41a7e26dba201c
shagags <seun2025@gmail.com>
2025-05-28 05:12:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-from fastapi import FastAPI
-from goldwick_api.app import app   # adjust if folder name differs
+import os
+from uvicorn import run
+import app  # or goldwick_api.app as app
+
+if __name__ == "__main__":
+    run("app:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
